cfff5ef1c103fc72ce04b5cbd8b9ace87cc2c151
solidstateiv <contactcharlerae@gmail.com>
2018-09-26 16:23:36 -0400

    Added buttons and Listeners and some html content

---
+++ elf.js
@@ -0,0 +1,74 @@
+
+
+var btn = document.getElementById('study');
+
+function blue(){
+    var rand = Math.floor(Math.random() * 10);
+    console.log(rand);
+    if (rand < 10) {
+        console.log("congrats");
+    } else {
+        console.log("sorry");
+    }
+
+//adding event listener/handler for school button
+btn.addEventListener('click',   
+
+);
+
+
+
+
+//printing result of school button
+if (adchoice = "congrats") {
+    console.log("Congrats!! You have a degree and are now a citizen of Vanhala, but you have another choice to make. War has broken out and you must decide if you will defend your new home.")
+} else if (sorry) {
+    console.log("Sorry.. You flunked out of school. Better luck next time grunt.")
+}
+
+
+
+
+//random outcome for market button
+var btn = document.getElementById('sell');
+
+//adding event listener for market button
+btn.addEventListener('click', function(){
+    var rand = Math.floor(Math.random() * choosetwo.length);
+    console.log(rand);
+})
+
+//printing result of market button
+if (adchoice = "congrats") {
+    console.log("You made BooKoo Bucks, paid your taxes and you are now a citizen of Vahnahla. However, you have another choice to make. War has broken out and you must decide if you will defend your new home.")
+} else (rand = 3) {
+    console.log("Sorry.. You got robbed at the market. Better luck next time grunt.")
+}
+
+
+//   XXXXX QQQ LEVEL TWO QQQ XXXXX
+
+
+//random outcome for market button
+function blah () {
+    var achoice = achoice [Math.floor(Math.random() * achoice.length)]
+};
+
+//printing result of war button
+if (cchoice < 2) {
+    console.log("Hooyaah!! You made it back from war! To reward you for helping us win this victory you have been invited into Vanhala's Noble class. On top of that -You Get To Meet The Queen. Careful though, she sometimes likes to make newcomers the butt of her jokes.")
+} else {
+    console.log("Game Over!!  Sorry.. You were lost at war. Better luck next time citizen.")
+}
+
+//random outcome for market button
+function blah () {
+    var achoice = achoice [Math.floor(Math.random() * achoice.length)]
+};
+
+//printing result of market button
+if (dchoice < 2) {
+    console.log("You Made Duckets on Duckets, and now You're RICH. As such you are now invited to become a memeber of Vahnahla's Nobility. On top of that -You Get To Meet The Queen. Careful though, she sometimes likes to make newcomers the butt of her jokes.")
+} else {
+    console.log("You Were Robbed. Sadly, you did not survive. Better luck next time citizen.")
+}
