7b7bb5cb56f7f552b525a322f4df3f855b53b90f
solidstateiv <contactcharlerae@gmail.com>
2018-10-01 15:41:02 -0400

    Updates to CSS

--- elf.js
+++ elf.js
@@ -1,79 +1,43 @@
 
 
-var btn = document.getElementById('study');
-
-study.addEventListener('click', function blue(){
+/* determining choice of first button  */
+school.addEventListener('click', function studychoice(){
     console.log(Math.floor(Math.random() * 10));
     
-     if (blue() < 6){
-     console.log("Congrats!! You have a degree and are now a citizen of Vanhala, but you have another choice to make. War has broken out and you must decide if you will defend your new home.");
-     }
-     else{
-     console.log("Sorry.. You flunked out of school. Better luck next time grunt.");
-     }
-  });
+if (studychoice() < 6){
+    onClick="javascript: windo.location.href = '/schoolcongrats'";
+    } else {
+        onClick="javascript: windo.location.href = '/schoolsorry'";
+    }
+});
 
-//adding event listener/handler for school button
-btn.addEventListener('click',  
-);
-
-
-//random outcome for market button
-var btn = document.getElementById('sell');
-var market = purple;
-
-//adding event listener for market button
-sell.addEventListener('click', function purple(){
+market.addEventListener('click', function marketchoice(){
     console.log(Math.floor(Math.random() * 10));
     
-    if (purple() < 6){
-      console.log("You made BooKoo Bucks, paid your taxes and you are now a citizen of Vahnahla. However, you have another choice to make. War has broken out and you must decide if you will defend your new home.");
+if (marketchoice() < 6){
+    onClick="javascript: windo.location.href = '/marketcongrats'";
+    } else {
+        onClick="javascript: windo.location.href = '/marketsorry'";
     }
-    else{
-      console.log("Sorry.. You got robbed at the market. Better luck next time grunt.");
+});
+
+/* determining choice of second button  */
+school.addEventListener('click', function warchoice(){
+    console.log(Math.floor(Math.random() * 10));
+    
+if (warchoice() < 6){
+    onClick="javascript: windo.location.href = '/warcongrats'";
+    } else {
+        onClick="javascript: windo.location.href = '/warsorry'";
     }
-  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   XXXXX QQQ LEVEL TWO QQQ XXXXX
-
-
-//random outcome for market button
-function blah () {
-    var achoice = achoice [Math.floor(Math.random() * achoice.length)]
-};
-
-//printing result of war button
-if (cchoice < 2) {
-    console.log("Hooyaah!! You made it back from war! To reward you for helping us win this victory you have been invited into Vanhala's Noble class. On top of that -You Get To Meet The Queen. Careful though, she sometimes likes to make newcomers the butt of her jokes.")
-} else {
-    console.log("Game Over!!  Sorry.. You were lost at war. Better luck next time citizen.")
-}
-
-//random outcome for market button
-function blah () {
-    var achoice = achoice [Math.floor(Math.random() * achoice.length)]
-};
-
-//printing result of market button
-if (dchoice < 2) {
-    console.log("You Made Duckets on Duckets, and now You're RICH. As such you are now invited to become a memeber of Vahnahla's Nobility. On top of that -You Get To Meet The Queen. Careful though, she sometimes likes to make newcomers the butt of her jokes.")
-} else {
-    console.log("You Were Robbed. Sadly, you did not survive. Better luck next time citizen.")
-}
+market.addEventListener('click', function queenchoice(){
+    console.log(Math.floor(Math.random() * 10));
+    
+if (queenchoice() < 6){
+    onClick="javascript: windo.location.href = '/armscongrats'";
+    } else {
+        onClick="javascript: windo.location.href = '/armssorry'";
+    }
+});
